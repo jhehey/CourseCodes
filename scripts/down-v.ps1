88c63344a077f644b18f2458be8e794c757faecc
jhehey <jhonpaul.jaspe@emerson.com>
2020-07-12 21:46:07 +0800

    Can Run docker with binded socket

---
+++ scripts/down-v.ps1
@@ -0,0 +1,6 @@
+echo "Stopping Services and Removing Volumes..."
+docker-compose down -v
+docker-compose ps
+echo "Stopping Containers..."
+docker stop $(docker ps -q)
+docker ps
--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -1,3 +1,8 @@
 $Env:PGPASSWORD="S2MognJhsZUnwY8LY8neenn2yUKmXMyLD5QTH9mFWhU"
+
+echo "Building Containers..."
 docker-compose up -d --build
+
+echo "Running Migrations..."
 .\migrate.ps1
+docker-compose ps
