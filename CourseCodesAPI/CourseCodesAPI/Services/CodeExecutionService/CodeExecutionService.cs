416d53ce096a575cb0001825f9d200e8eab74b1f
jhehey <jhonpaul.jaspe@emerson.com>
2020-07-27 17:22:19 +0800

    Fix cut output:2

--- CourseCodesAPI/CourseCodesAPI/Services/CodeExecutionService/CodeExecutionService.cs
+++ CourseCodesAPI/CourseCodesAPI/Services/CodeExecutionService/CodeExecutionService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using CourseCodesAPI.Helpers;
 using CourseCodesAPI.Services.CodeExecutionService.Factories;
